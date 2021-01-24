b1b9422d75dfb77ef78f0b4f3ad17e0d99dc6a73
francisgill42 <francisgill42@gmail.com>
2021-01-25 00:07:09 +0500

    fixed index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,30 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+
+  <v-row class="mt-5">
+    <v-col sm="12" v-for="(bd,i) in btnData" :key="i" cols="12" class="text-center">
+       
+         <a target="blank" style="text-decoration:none;" class="white--text" :href="whatsappURL + number">
+             <v-btn width="50%" x-large color="success accent-4" class="success"> {{bd.text}} </v-btn>
+         </a>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+layout : 'empty',
+data : () => ({
+    whatsappURL : 'https://wa.me/',
+    number : '+923108559858',
+    btnData : [
+        {'text' : 'Electrician'},
+        {'text' : 'Plumber'},
+        {'text' : 'Painter'},
+        {'text' : 'Mechanic'},
+        {'text' : 'Labour'},
+    ] 
+})    
 }
 </script>
